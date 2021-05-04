030e0a225a1de080ebb29df387a32258a45900e2
Md Salman <md.salman@alphasq.com>
2021-05-04 12:13:39 +0600

    Home Screen populated with fake product data

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className="py-3">
+        <Container>
+        <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
